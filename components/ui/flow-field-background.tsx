1465974fb1129fe63c881d31e4e8dba6fdb9909b
Vamshi Billa <vamshibilla93@gmail.com>
2026-02-21 12:03:01 +0530

    partical add

--- components/ui/flow-field-background.tsx
+++ components/ui/flow-field-background.tsx
@@ -36,7 +36,13 @@ export default function NeuralBackground({
     let animationFrameId: number;
     let mouse = { x: -1000, y: -1000 };
 
-    
+    class Particle {
+      x: number;
+      y: number;
+      vx: number;
+      vy: number;
+      age: number;
+      life: number;
 
       constructor() {
         this.x = Math.random() * width;
