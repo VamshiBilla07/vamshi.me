9d4faded7ffde4cb57092e8f0f0144a2b7654c74
VamshiBilla07 <vamshibilla93@gmail.com>
2026-03-02 18:00:53 +0530

    Increase size of skill icons in skills route

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -21,42 +21,45 @@ export default function SkillsPage() {
       <div className="flex flex-row gap-70 justify-center items-start z-10">
         {/* Column 1 */}
         <div className="flex flex-col gap-6">
-          <img src="https://skillicons.dev/icons?i=html" alt="HTML" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=css" alt="CSS" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=js" alt="JavaScript" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=ts" alt="TypeScript" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=react" alt="React" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=next" alt="Next.js" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=python" alt="Python" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=java" alt="Java" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=cpp" alt="C++" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=git" alt="Git" className="h-8" />
+
+          <img src="https://skillicons.dev/icons?i=html" alt="HTML" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=css" alt="CSS" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=js" alt="JavaScript" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=ts" alt="TypeScript" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=react" alt="React" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=cpp" alt="C++" className="h-14" />
+          
         </div>
         {/* Column 2 */}
         <div className="flex flex-col gap-6">
-          <img src="https://skillicons.dev/icons?i=github" alt="GitHub" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=tailwind" alt="Tailwind CSS" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=flutter" alt="Flutter" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=kubernetes" alt="Kubernetes" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=npm" alt="NPM" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=aws" alt="AWS" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=googlecloud" alt="Google Cloud" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=firebase" alt="Firebase" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=mongodb" alt="MongoDB" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=postman" alt="Postman" className="h-8" />
+          <img src="https://skillicons.dev/icons?i=github" alt="GitHub" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=tailwind" alt="Tailwind CSS" className="h-14" />
+          {/* <img src="https://skillicons.dev/icons?i=flutter" alt="Flutter" className="h-14" /> */}
+          <img src="https://skillicons.dev/icons?i=kubernetes" alt="Kubernetes" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=python" alt="Python" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=java" alt="Java" className="h-14" />
+          {/* <img src="https://skillicons.dev/icons?i=npm" alt="NPM" className="h-14" /> */}
+          <img src="https://skillicons.dev/icons?i=mongodb" alt="MongoDB" className="h-14" />
+          {/* <img src="https://skillicons.dev/icons?i=postman" alt="Postman" className="h-14" /> */}
         </div>
         {/* Column 3 */}
         <div className="flex flex-col gap-6">
-          <img src="https://skillicons.dev/icons?i=nodejs" alt="Node.js" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=expressjs" alt="Express.js" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=bootstrap" alt="Bootstrap" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=vite" alt="Vite" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=postgresql" alt="PostgreSQL" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=mysql" alt="MySQL" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=docker" alt="Docker" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=vercel" alt="Vercel" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=vue" alt="Vue.js" className="h-8" />
-          <img src="https://skillicons.dev/icons?i=php" alt="PHP" className="h-8" />
+          {/* <img src="https://skillicons.dev/icons?i=nodejs" alt="Node.js" className="h-14" /> */}
+          {/* <img src="https://skillicons.dev/icons?i=expressjs" alt="Express.js" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=bootstrap" alt="Bootstrap" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=vite" alt="Vite" className="h-14" /> */}
+          {/* <img src="https://skillicons.dev/icons?i=postgresql" alt="PostgreSQL" className="h-14" /> */}
+          <img src="https://skillicons.dev/icons?i=mysql" alt="MySQL" className="h-14" />
+          {/* <img src="https://skillicons.dev/icons?i=docker" alt="Docker" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=vercel" alt="Vercel" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=vue" alt="Vue.js" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=php" alt="PHP" className="h-14" /> */}
+          <img src="https://skillicons.dev/icons?i=next" alt="Next.js" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=git" alt="Git" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=aws" alt="AWS" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=googlecloud" alt="Google Cloud" className="h-14" />
+          <img src="https://skillicons.dev/icons?i=firebase" alt="Firebase" className="h-14" />
+          
         </div>
       </div>
     </main>
